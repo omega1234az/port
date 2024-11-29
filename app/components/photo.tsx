6ce075925411d13a84470bcf3b3f84de93c6e139
Suphakit Ranphol <supakit0920@gmail.com>
2024-11-29 20:17:35 +0700

    เปลี่ยนโครงสร้างไฟล์

---
+++ app/components/photo.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function Photo() {
+  return (
+    <>
+      <div className="container mx-auto mt-4">
+      <p className="text-xl font-bold p-5">Photo</p>
+        <div className="grid grid-cols-10 gap-5">
+          
+
+        </div>
+      </div>
+    </>
+  );
+}
