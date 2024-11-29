4dd81621d7501345b10d0f064bc45d1b276ef2c6
Suphakit Ranphol <supakit0920@gmail.com>
2024-11-29 20:38:01 +0700

    photo change

--- app/components/photo.tsx
+++ app/components/photo.tsx
@@ -10,42 +10,42 @@ export default function Photo() {
               src="/photo/k-on.gif"
               width={500}
               height={500}
-              
+              alt="k-on"
               className="py-2 rounded-2xl"
             />
              <Image
               src="/photo/bocchii.gif"
               width={500}
               height={200}
-              
+              alt="bocchi"
               className="py-2 rounded-2xl"
             />
              <Image
               src="/photo/byou.gif"
               width={500}
               height={500}
-              
+              alt="byou"
               className="py-2 rounded-2xl"
             />
              <Image
               src="/photo/furry.gif"
               width={500}
               height={500}
-              
+              alt="furry"
               className="py-2 rounded-2xl"
             />
              <Image
               src="/photo/gojo.gif"
               width={500}
               height={500}
-              
+              alt="gojo"
               className="py-2 rounded-2xl"
             />
            <Image
               src="/photo/nagatoro.gif"
               width={500}
               height={500}
-              
+              alt="nagatoro"
               className="py-2 rounded-2xl"
             />
         </div>
